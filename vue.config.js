486326c4f1342beff74a1c4cdce06c3a363f121b
saeed <developer0008@gmail.com>
2023-08-12 09:13:43 +0300

    update sweetalert library

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ],
-  chainWebpack: config => {
-    config.module.rules.delete('eslint');
-  }
+  ]
 }
 process.env.VUE_CLI_TEST = false
